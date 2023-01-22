826d3bd34c4e1087353734691117c850278b1aec
Shubh <shubh.scb@gmail.com>
2023-01-22 18:44:06 +0530

    NEW PROJECT ADD.

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -2,6 +2,7 @@ import dream from '../../assets/images/project-screenshoot/dream.png';
 import corona from '../../assets/images/project-screenshoot/corona.png';
 import math from '../../assets/images/project-screenshoot/math.png';
 import space from '../../assets/images/project-screenshoot/space.png';
+import webdev from '../..assets/images/project-screenshoot/webdev.png';
 
 const PortfolioData = [
   {
@@ -43,6 +44,17 @@ const PortfolioData = [
     description: 'Space Travelers is a web application that allows users to book rockets and join selected space missions for space travel.',
     seelive: 'https://travelers-hub-rich-shubh.netlify.app/',
     seesource: 'https://github.com/assadounto/space-x.git',
+  },
+  {
+    id: 5,
+    category: 'web site',
+    tech: 'JavaScript | CSS | Html',
+    title: 'Web development school',
+    image: webdev,
+    describtion: 'A website development school website. Users can get information about new admission, faculties, fees, course details, etc.',
+    seelive: 'https://shubh-dev.github.io/The-Capstone/',
+    seesource: 'https://github.com/Shubh-Dev/Web-Development-Training-School.git',
+
   },
 ];
 export default PortfolioData;
